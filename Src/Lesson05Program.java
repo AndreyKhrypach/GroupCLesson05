2614902928131541adc7eb95c075780a44a3ed80
AndreyKhrypach <pacik78@gmail.com>
2023-10-13 21:37:11 +0300

    firs commit

---
+++ Src/Lesson05Program.java
@@ -0,0 +1,130 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson05Program {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("введіть число в діапазоні до 10: ");
+//        int manualInt = scanner.nextInt();
+//        System.out.println(manualInt);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Введіть назву команди: ");
+            String team = scanner.nextLine();
+            System.out.println(team);
+        }
+
+//        Random random = new Random();
+//
+//        int[] manualArr = new int[]{6, 3, 23, 1, 3};
+//        System.out.println(Arrays.toString(manualArr));
+//        for (int index = 0; index < manualArr.length; index++) {
+//            if(manualArr[index] == 3){
+//                System.out.println(manualArr[index] + " : " + index);
+//            }
+//        }
+//        Arrays.sort(manualArr);
+//        System.out.println(Arrays.toString(manualArr));
+////        for (int index = manualArr.length - 1; index >= 0 ; index--) {
+////            System.out.print(manualArr[index] + ", ");
+////        }
+//        System.out.println();
+
+//        int[] arr = new int[5];
+//        arr[0] = 1;
+//        arr[1] = 55;
+//        arr[2] = 105;
+//        arr[3] = 4;
+//        arr[4] = 3;
+//
+//        int sum = 0;
+//        for (int arrElement:
+//             arr) {
+//            sum += arrElement;
+//            System.out.println(arrElement);
+//        }
+//        double avg = (double) sum / arr.length;
+//        System.out.println(avg + ": " + sum);
+//        int[] manualArrayInt = new int[]{1, 5, 33, 2, 8};
+//        int[] manualArrayInt2 = {1, 5, 33, 2, 8};
+//        System.out.println(manualArrayInt.length);
+//        System.out.println(Arrays.toString(manualArrayInt));
+//
+//        for (int index = 0; index < manualArrayInt.length; index++) {
+//            System.out.print(manualArrayInt[index] + ", ");
+//        }
+//        System.out.println();
+//        int[] arrayInt = new int[5];
+//        byte[] arrayByte = new byte[5];
+//        short[] arrayShort = new short[5];
+//        long[] arrayLong = new long[5];
+//        float[] arrayFloat = new float[5];
+//        double[] arrayDouble = new double[5];
+//        boolean[] arrayBoolean = new boolean[5];
+//        for (int index = 0; index < arrayInt.length; index++) {
+//            System.out.println(arrayInt[index] + " " + arrayBoolean[index]);
+//        }
+//        for (int index = 0; index < arrayInt.length ; index++) {
+//            arrayInt[index] = random.nextInt(10);
+//        }
+//        for (int index = 0; index < arrayInt.length; index++) {
+//            System.out.println(arrayInt[index] + ": " + index);
+//        }
+//        int a = 0;
+//
+//        while (true){
+//            System.out.println(a);
+//            a++;
+//            if(a > 100){
+//                break;
+//            }
+//        }
+
+//        for ( ; ; ) {
+//
+//            System.out.println(a);
+//            if(a == 100){
+//                break;
+//            }
+//            a++;
+//        }
+
+
+//        int a = 10;
+
+//        do {
+//            System.out.println(a);
+//            a++;
+//        }while(a < 11);
+
+
+//        int a = 0;
+//
+//        boolean bool = false;
+//
+//        while (!bool){
+//            System.out.println(a);
+//            a++;
+//            if(a == 10){
+//                bool = true;
+//            }
+//        }
+
+//        int a = 0;
+//        while(a < 10){
+//            System.out.println(a);
+//            a++;
+//        }
+
+        //int randomNum = 0;
+        //int i = 0;
+//        for (int i = 1; i < 10; i+=10) {
+//
+//            System.out.println(i);
+//           // randomNum++;
+//        }
+//        System.out.println("______________________________");
+        // System.out.println(i);
+    }
+}
